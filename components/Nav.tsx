c4aab849e57a61b01091fa673d67e8c43fdd65f8
gabebraden <gabe.braden@solace.com>
2023-11-18 22:52:23 -0500

    Basic splash page

---
+++ components/Nav.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Image from "next/image";
+import { ModeToggle } from "./ui/toggle-mode";
+
+export default function Nav() {
+  return (
+    <header>
+      <nav>
+        <ul className="flex items-center justify-between">
+          <li>
+            <Image
+              src="/retell.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </li>
+          <li>
+            <ModeToggle />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
